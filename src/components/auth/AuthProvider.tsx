43f2998e70dd8b77a445490423e72fb9dba89e11
MD. Rakib <rakib260.hossain@gmail.com>
2026-05-05 01:17:04 +0600

    auth provider check session implement

--- src/components/auth/AuthProvider.tsx
+++ src/components/auth/AuthProvider.tsx
@@ -1,30 +1,67 @@
 'use client'
 import { supabase } from '@/lib/supabaseClient'
 import { setProfile, setUser } from '@/redux/auth/authSlice'
-import { useAppDispatch} from '@/redux/hooks'
+import { useAppDispatch } from '@/redux/hooks'
 import React, { useEffect } from 'react'
+
 function AuthProvider({ children }: { children: React.ReactNode }) {
   const dispatch = useAppDispatch()
+  
   useEffect(() => {
-  const {data:listener}=supabase.auth.onAuthStateChange( async(event,session)=>{
-    console.log('auth event:',event)
-    if(session?.user){
-      dispatch(setUser(session.user))
-      const {data:profile}= await supabase.from('profiles').select('*').eq('id',session.user.id).single()
-      if(profile){
-        dispatch(setProfile(profile))
-      }else{
+    const checkSession = async () => {
+      const { data: { session } } = await supabase.auth.getSession()
+      
+      if (session?.user) {
+        dispatch(setUser(session.user))
+        const { data: profile } = await supabase
+          .from('profiles')
+          .select('*')
+          .eq('id', session.user.id)
+          .single()
+        
+        if (profile) {
+          dispatch(setProfile(profile))
+        } else {
+          dispatch(setUser(null))
+          dispatch(setProfile(null))
+        }
+      } else {
         dispatch(setUser(null))
         dispatch(setProfile(null))
       }
     }
     
-  })
-  return ()=>{
-    listener.subscription.unsubscribe()
-  }
- 
+    checkSession()
+    const { data: { subscription } } = supabase.auth.onAuthStateChange(
+      async (event, session) => {
+        console.log('auth event:', event)
+        
+        if (session?.user) {
+          dispatch(setUser(session.user))
+          const { data: profile } = await supabase
+            .from('profiles')
+            .select('*')
+            .eq('id', session.user.id)
+            .single()
+          
+          if (profile) {
+            dispatch(setProfile(profile))
+          } else {
+            dispatch(setUser(null))
+            dispatch(setProfile(null))
+          }
+        } else {
+          dispatch(setUser(null))
+          dispatch(setProfile(null))
+        }
+      }
+    )
+    
+    return () => {
+      subscription.unsubscribe()
+    }
   }, [dispatch])
+  
   return <>{children}</>
 }
 
